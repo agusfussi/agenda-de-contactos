f5b0969a62ea2e0bbe3143eaf7ffb27c07cd84ab
agusfussi <agusfussiwor@gmail.com>
2025-09-02 18:15:21 -0300

    Hice la side bar colapsable basada en una de w3school

--- src/app/layouts/logged-layouts/logged-layouts.ts
+++ src/app/layouts/logged-layouts/logged-layouts.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-logged-layouts',
-  imports: [RouterOutlet],
+  standalone: true,                    // <- importante si usás imports en el decorator
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './logged-layouts.html',
-  styleUrl: './logged-layouts.scss'
+  styleUrls: ['./logged-layouts.scss']  // <- nota: "styleUrls" (plural)
 })
 export class LoggedLayouts {
+  // estado y métodos del sidebar (ahora están en la misma clase que la plantilla)
+  isSidebarOpen = false;
 
+  openNav() {
+    this.isSidebarOpen = true;
+  }
+
+  closeNav() {
+    this.isSidebarOpen = false;
+  }
 }
