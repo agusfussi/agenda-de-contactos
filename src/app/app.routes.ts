b3622334452e8ae9c54ec37fb9f6c31c379909ab
agusfussi <agusfussiwor@gmail.com>
2025-09-17 09:48:09 -0300

    gurad hecho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,33 +4,39 @@ import { ContactPages } from './pages/contact-pages/contact-pages';
 import { ContactDetailsPage } from './pages/contact-details-page/contact-details-page';
 import { LoggedLayouts } from './layouts/logged-layouts/logged-layouts';
 import { Register } from './pages/register/register';
+import { onlyPublicUserGuard } from './guards/only-public-user-guard';
+import { onlyLoggedUserGuard } from './guards/only-logged-user-guard';
 //import { Register } from './pages/register/register';
 
 export const routes: Routes = [
     {
         path: "login",
-        component: Login
+        component: Login,
+        canActivate: [onlyPublicUserGuard]
     },
     {
-        path:"register",
-        component:Register
+        path: "register",
+        component: Register,
+        canActivate: [onlyPublicUserGuard]
     },
     {
-        path:"",
-        component:LoggedLayouts,
-        children:[
-                {
-        path:"contact-pages",
-        component:ContactPages
-    },
-    {
-        path:"contact-details",
-        component:ContactDetailsPage
-    },
+        path: "",
+        component: LoggedLayouts,
+        children: [
+            {
+                path: "contact-pages",
+                component: ContactPages,
+                canActivateChild: [onlyLoggedUserGuard]
+            },
+            {
+                path: "contact-details",
+                component: ContactDetailsPage,
+                canActivateChild: [onlyLoggedUserGuard]
+            },
         ]
     },
     // {
-        //path:"register-pages",
-        //component:Register
+    //path:"register-pages",
+    //component:Register
     // },
 ];
---
+++ src/app/guards/only-public-user-guard.ts
@@ -0,0 +1,16 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router, RedirectCommand } from '@angular/router';
+import { AuthService } from '../services/auth-service';
+
+export const onlyPublicUserGuard: CanActivateFn = (route, state) => {
+  const auth = inject(AuthService)
+  const router = inject(Router)
+
+  if(auth.token){
+    const loginPath = router.parseUrl("/");
+      return new RedirectCommand(loginPath, {
+        skipLocationChange: true,
+      });
+  }
+  return true;
+};
