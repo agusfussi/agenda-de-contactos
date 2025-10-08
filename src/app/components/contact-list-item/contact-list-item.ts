4b462d558cfae96a6b8c7aa8e1afda0a6ce970b4
agusfussi <agusfussiwor@gmail.com>
2025-10-08 10:10:24 -0300

    clase 8/10

--- src/app/components/contact-list-item/contact-list-item.ts
+++ src/app/components/contact-list-item/contact-list-item.ts
@@ -2,6 +2,8 @@ import { Component, inject, input } from '@angular/core';
 import { Contact } from '../../interfaces/contact';
 import { ContactsService } from '../../services/contacts-service';
 import { RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-contact-list-item',
@@ -10,7 +12,24 @@ import { RouterModule } from '@angular/router';
   styleUrl: './contact-list-item.scss'
 })
 export class ContactListItem {
- contacto = input.required<Contact>()
+  contacto = input.required<Contact>()
 
- contactService = inject(ContactsService)
+  contactService = inject(ContactsService)
+  opendeletemodal() {
+    Swal.fire({
+      title: "Vas a borrar el contacto?",
+      showDenyButton: true,
+      confirmButtonText: "Eliminar",
+      denyButtonText: `No eliminar`
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        this.contactService.deleteContact(this.contacto().id).then(() => {
+          Swal.fire("Contacto eliminado correctamente", "", "success");
+        });
+      } else if (result.isDenied) {
+        Swal.fire("No se a borrado el contacto", "", "info");
+      }
+    });
+  }
 }
--- src/app/layouts/logged-layouts/logged-layouts.ts
+++ src/app/layouts/logged-layouts/logged-layouts.ts
@@ -2,17 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
+import Swal from 'sweetalert2'
+
 
 @Component({
   selector: 'app-logged-layouts',
-  standalone: true,                    
+  standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './logged-layouts.html',
-  styleUrls: ['./logged-layouts.scss'] 
+  styleUrls: ['./logged-layouts.scss']
 })
 export class LoggedLayouts {
   // estado y métodos del sidebar (ahora están en la misma clase que la plantilla)
+
   isSidebarOpen = false;
+
   authService = inject(AuthService)
 
   openNav() {
@@ -22,4 +26,21 @@ export class LoggedLayouts {
   closeNav() {
     this.isSidebarOpen = false;
   }
+  openLogoutModal() {
+    Swal.fire({
+      title: "Vas a deslogearte?",
+      showDenyButton: true,
+      confirmButtonText: "Deslogear",
+      denyButtonText: `Quedarme`
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        this.authService.logeout();
+        Swal.fire("Deslogueado", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("No te has deslogueado", "", "info");
+      }
+    });
+  }
 }
+
