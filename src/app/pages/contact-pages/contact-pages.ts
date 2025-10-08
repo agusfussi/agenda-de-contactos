346f674c07d17d7e2fd656bc0d26bb7c35337ea4
agusfussi <agusfussiwor@gmail.com>
2025-10-08 11:12:16 -0300

    Routerlink

--- src/app/pages/contact-pages/contact-pages.ts
+++ src/app/pages/contact-pages/contact-pages.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 import { ContactListItem } from '../../components/contact-list-item/contact-list-item';
 import { AuthService } from '../../services/auth-service';
 import { ContactsService } from '../../services/contacts-service';
@@ -9,7 +9,7 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-pages',
-  imports: [RouterModule, ContactListItem, FormsModule],
+  imports: [RouterModule, ContactListItem, FormsModule, RouterLink],
   templateUrl: './contact-pages.html',
   styleUrl: './contact-pages.scss'
 })
